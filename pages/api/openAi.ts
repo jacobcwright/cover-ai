59f641a66876dd84453f610f25dc11f4f62f4b0e
Jacob Wright <jacob.wright.ut@gmail.com>
2022-10-18 21:43:55 -0600

    CHORE: try catch

--- pages/api/openAi.ts
+++ pages/api/openAi.ts
@@ -9,22 +9,26 @@ const openai = new OpenAIApi(configuration)
 // create handler function that calls the openai api and returns the response
 const handler = async (req: any, res: any) => {
   const { prompt } = req.body
-  const response: AxiosResponse<CreateCompletionResponse, any> =
-    await openai.createCompletion({
-      model: "text-davinci-002",
-      prompt,
-      max_tokens: 100,
-      temperature: 0.5,
-      top_p: 1,
-      n: 1,
-      stream: false,
-      logprobs: null,
-      presence_penalty: 0,
-      frequency_penalty: 0,
-      best_of: 1,
-      user: "",
-    })
-  res.status(200).json(response.data)
+  try {
+    const response: AxiosResponse<CreateCompletionResponse, any> =
+      await openai.createCompletion({
+        model: "text-davinci-002",
+        prompt,
+        max_tokens: 100,
+        temperature: 0.5,
+        top_p: 1,
+        n: 1,
+        stream: false,
+        logprobs: null,
+        presence_penalty: 0,
+        frequency_penalty: 0,
+        best_of: 1,
+        user: "",
+      })
+    res.status(200).json(response.data)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 export default handler

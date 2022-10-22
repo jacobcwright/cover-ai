4656ae0caa8bf22240985e355ffa7f796cf794db
Jacob Wright <jacob.wright.ut@gmail.com>
2022-10-21 22:46:01 -0600

    FIX: remove unnecessary imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next"
 import Head from "next/head"
 import { useState } from "react"
-import styles from "../styles/Home.module.css"
 
 const Home: NextPage = () => {
   const [name, setName] = useState("")

0f1b4cd39bbb6fb598d6a069ab6ef5fb897143fe
Jacob Wright <jacob.wright.ut@gmail.com>
2022-10-23 14:24:26 -0600

    FEAT: ui for login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,21 +1,80 @@
-import React from "react"
+/* eslint-disable react/no-unescaped-entities */
+import Image from "next/image"
+import { useRouter } from "next/router"
+import React, { useState } from "react"
 
 function Login() {
+  const router = useRouter()
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [emailError, setEmailError] = useState("")
+
   return (
-    <div className="flex w-full h-full items-center justify-center align-middle">
-      <div className="flex flex-col justify-center items-center w-1/2 h-full align-middle">
-        <label className="text-2xl">Login</label>
-        <input
-          className="w-full p-4 rounded-md border-2 border-gray-300"
-          placeholder="Email"
-        />
-        <input
-          className="w-full p-4 rounded-md border-2 border-gray-300"
-          placeholder="Password"
-        />
-        <button className="bg-red-300 text-white text-2xl px-8 py-4 rounded-md">
-          Submit
-        </button>
+    <div className="flex w-full h-full items-center justify-center align-middle px-8 text-black">
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="px-8 py-6 mt-4 text-left bg-slate-300 shadow-lg rounded-xl">
+          <div className="flex justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-20 h-20 text-blue-600"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path d="M12 14l9-5-9-5-9 5 9 5z" />
+              <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+              />
+            </svg>
+          </div>
+          <h3 className="text-2xl font-bold text-center">
+            Login or create an account
+          </h3>
+          <form action="">
+            <div className="mt-4">
+              <div>
+                <label className="block" htmlFor="email">
+                  Email
+                </label>
+                <input
+                  type="text"
+                  placeholder="Email"
+                  className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600 bg-white"
+                />
+                {emailError && (
+                  <span className="text-xs tracking-wide text-red-600">
+                    Email field is required{" "}
+                  </span>
+                )}
+              </div>
+              <div className="mt-4">
+                <label className="block">Password</label>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600 bg-white"
+                />
+              </div>
+              <div className="flex items-baseline justify-between">
+                <button
+                  className="px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900"
+                  onClick={() => {
+                    router.push("/chicken")
+                  }}
+                >
+                  Login
+                </button>
+                <a href="#" className="text-sm text-blue-600 hover:underline">
+                  Forgot password?
+                </a>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   )

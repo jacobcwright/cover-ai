37bf85fe870d3cf9c2edbe9192f70de626d027ad
Jacob Wright <jacob.wright.ut@gmail.com>
2022-10-25 17:06:19 -0600

    FIX: amplify import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.scss"
 import type { AppProps } from "next/app"
-import Amplify from "aws-amplify"
+import { Amplify } from "aws-amplify"
 import config from "../src/aws-exports"
 Amplify.configure(config)
 

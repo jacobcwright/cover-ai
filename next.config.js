09109c16b46d3cb21b8d1e8485657c3e3252ec66
Jacob Wright <jacob.wright.ut@gmail.com>
2022-10-25 21:07:27 -0600

    FIX: env var

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // add env variable
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
 }
 
 module.exports = nextConfig

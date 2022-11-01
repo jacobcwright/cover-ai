e28e0648060ea1621ee7fcad9e3ee1143e561903
Jacob Wright <jacob.wright.ut@gmail.com>
2022-11-01 17:01:10 -0600

    feat/header

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Header() {
+  return (
+    <div className="w-full mt-6">
+      <h1 className="flex font-[Averia-Serif-Libre] text-3xl text-[#015369] w-full">
+        Br<p className="text-[#0BA8D3]">ai</p>niac
+      </h1>
+    </div>
+  )
+}
+
+export default Header

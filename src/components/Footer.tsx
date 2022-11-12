8a79e948df3252363cfd4dab85c4d4b69d288b49
Jacob Wright <jacob.wright.ut@gmail.com>
2022-11-12 14:15:51 -0700

    fix/ footer full width on mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 function Footer({}: Props) {
   return (
-    <div className="flex w-full h-24 bg-[#9EB7BE] font-[Averia-Serif-Libre] justify-end content-center items-center px-8">
+    <div className="flex w-[100vw] md:w-full h-24 bg-[#9EB7BE] font-[Averia-Serif-Libre] justify-end content-center items-center px-8">
       <h1 className="flex text-2xl text-white tracking-wider">
         © 2022 Br<p className="text-[#0BA8D3]">ai</p>niac
       </h1>
